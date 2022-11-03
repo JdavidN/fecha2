96bd911633ac8fd25c13fae4ceedf8f522a30b17
Javier David Nino Ruiz <103191427+JdavidN@users.noreply.github.com>
2022-11-03 07:59:10 -0500

    Add files via upload

--- ejecutable/Test.java
+++ ejecutable/Test.java
@@ -1,1 +1,39 @@
+package ejecutable;
 
+import modelo.Fecha;
+
+public class Test 
+{
+ public static void main(String[] args) 
+ {
+    Fecha f= new Fecha();
+    f.setDia(2);
+    f.setMes(10);
+    f.setAnio(1970);
+
+    //mostrar en consola
+
+    System.out.println("Dia: " + f.getDia());
+    System.out.println("Mes: " + f.getMes());
+    System.out.println("Año: " + f.getAnio());
+    System.out.println(f.toString());
+
+    Fecha f2 = new Fecha(8, 10, 2020);
+    System.out.println(f2.toString());
+
+
+    
+
+
+    if(f.equals(f2))
+    {
+        System.out.println("Las fechas son iguales");
+    }
+    else
+    {
+        System.out.println("Las fechas son Diferentes");
+    }
+    
+    
+ }    
+}
